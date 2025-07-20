1075cbc1805a0244f9412a51ae43e3c86ba22ebe
Ivonnela <ivonne.nani14@gmail.com>
2025-07-19 21:40:49 -0500

    Delete Eval.Integral directory

--- Eval.Integral/script.js
+++
@@ -1,5 +0,0 @@
-// Validación simple
-document.getElementById('formContacto').addEventListener('submit', function(e) {
-  alert("Gracias por tu mensaje. Nos pondremos en contacto pronto.");
-  e.preventDefault();
-});
