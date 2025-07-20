d5ec462c25ea39630c286ae0eb2c96cccc675415
Ivonnela <ivonne.nani14@gmail.com>
2025-07-19 21:41:26 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Validación simple
+document.getElementById('formContacto').addEventListener('submit', function(e) {
+  alert("Gracias por tu mensaje. Nos pondremos en contacto pronto.");
+  e.preventDefault();
+});
